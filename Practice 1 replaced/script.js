072da8b649191f5d40539e506e6f990fff770494
Alish Goroglu <Alishgoroglu@hotmail.com>
2025-09-23 22:57:12 +0300

    Update script.js

--- Practice 1 replaced/script.js
+++ Practice 1 replaced/script.js
@@ -1,21 +1,25 @@
+// Selects all elements with the class 'panel' and stores them in the 'panels' constant.
 const panels = document.querySelectorAll('.panel')
 
+// Iterates over each 'panel' element in the 'panels' NodeList.
 panels.forEach(panel => {
 
+// Adds a 'click' event listener to each 'panel'.
 panel.addEventListener('click', () => {
     
-
+    // Calls the 'removeActive' function to remove the 'active' class from all panels.
     removeActive();
+    // Adds the 'active' class to the specific panel that was clicked.
     panel.classList.add("active");
 
-
-
 })
 })
 
-
+// Defines a function to remove the 'active' class from all panels.
 function removeActive() {
+    // Iterates over each 'panel' element again.
     panels.forEach(panel => {
+        // Removes the 'active' class from the current panel.
         panel.classList.remove("active");
     })
 }
